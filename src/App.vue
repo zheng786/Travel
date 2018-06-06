33a38cbe4709f83934efeee9f58b026c04e2fcb3
Zpp <617990472@qq.com>
2018-06-06 18:48:21 +0800

    finsh1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <!--router-view 显示的是当前路由地址所对应的内容-->
-=======
     <img src="./assets/logo.png">
->>>>>>> bd28540ddcaa4544ee41c99ad82736abac04e1b1
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
-  name: 'Home'
-=======
   name: 'App'
->>>>>>> bd28540ddcaa4544ee41c99ad82736abac04e1b1
 }
 </script>
 
 <style>
-<<<<<<< HEAD
-
-=======
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -31,5 +21,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
->>>>>>> bd28540ddcaa4544ee41c99ad82736abac04e1b1
 </style>
