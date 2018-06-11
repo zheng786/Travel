5f63e4aea1e34c0ca28654d41457bd623a8b365e
Zpp <617990472@qq.com>
2018-06-11 16:44:20 +0800

    finish city css

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <!--当前城市-->
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+        </div>
+      </div>
+      <!--热门城市-->
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">上海</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">天津</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">南京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">天津</div>
+          </div>
+        </div>
+      </div>
+      <!--A城市-->
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <!--B城市-->
+      <div class="area">
+        <div class="title border-topbottom">B</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <!--C城市-->
+      <div class="area">
+        <div class="title border-topbottom">C</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    name:'CityList',
+    mounted () {
+      this.scroll = new BScroll(this.$refs.wrapper)
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus" scoped>
+  @import "~styles/varibles.styl"
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    left 0
+    top 1.58rem
+    right 0
+    bottom 0
+    .title
+      text-align left
+      line-height .54rem
+      background #eee
+      padding-left .2rem
+      color #666
+      font-size .26rem
+    .button-list
+      //触动BFC事件，清除浮动
+      overflow hidden
+      padding .1rem .6rem .1rem .1rem
+      .button-wrapper
+        width 33.33%
+        float left
+        .button
+          margin .1rem
+          padding .1rem 0
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+      .item
+        line-height .76rem
+        color #666
+        padding-left .2rem
+        text-align left
+</style>
